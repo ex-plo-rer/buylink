ba38d7bf236cef36bef8ef48a709432e8266382c
Joseph Deborah Oluwabunmi <josephdeborah506@gmail.com>
2022-06-03 12:32:09 +0100

    chat ui and camera feature implementation

---
+++ lib/widgets/message_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+Widget messageList() {
+  return Flexible(
+    child:
+            ListView(
+              padding: EdgeInsets.all(10),
+
+            ));
+          }
+
