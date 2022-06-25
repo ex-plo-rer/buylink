e0981111813cd5880f0d3903e28bf4d1be74a106
Tobiloba <developertobi@gmail.com>
2022-06-25 16:18:18 +0100

    Added the preloader

--- lib/core/utilities/loader.dart
+++ lib/core/utilities/loader.dart
@@ -1,3 +1,6 @@
+import 'package:buy_link/core/constants/colors.dart';
+import 'package:buy_link/core/constants/images.dart';
+import 'package:buy_link/widgets/spacing.dart';
 import 'package:flutter/material.dart';
 
 class Loader {
@@ -8,29 +11,53 @@ class Loader {
   showLoader({
     required String text,
   }) {
-    AlertDialog alert = AlertDialog(
-      content: text.isEmpty
-          ? const SizedBox(
-              height: 50,
-              width: 50,
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            )
-          : Row(
-              children: [
-                const CircularProgressIndicator(),
-                Container(
-                    margin: const EdgeInsets.only(left: 7),
-                    child: Text("$text...")),
-              ],
-            ),
-    );
+    // AlertDialog alert = AlertDialog(
+    //     actionsPadding: EdgeInsets.zero,
+    //     buttonPadding: EdgeInsets.zero,
+    //     contentPadding: EdgeInsets.zero,
+    //     insetPadding: EdgeInsets.zero,
+    //     titlePadding: EdgeInsets.zero,
+    //     content: Column(
+    //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //       children: [
+    //         Padding(
+    //           padding: const EdgeInsets.only(top: 50),
+    //           child: Image.asset(AppImages.staticLoader),
+    //         ),
+    //         Image.asset(AppImages.loader),
+    //         const Spacing.empty(),
+    //       ],
+    //     ));
     showDialog(
       barrierDismissible: false,
       context: context,
       builder: (BuildContext context) {
-        return WillPopScope(onWillPop: () async => false, child: alert);
+        // return WillPopScope(onWillPop: () async => false, child: alert);
+        return WillPopScope(
+          onWillPop: () async => false,
+          child: Container(
+            decoration: const BoxDecoration(color: Color(0xffE5E5E5)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 50),
+                  child: Image.asset(
+                    AppImages.staticLoader,
+                    width: 28,
+                    height: 25,
+                  ),
+                ),
+                Image.asset(
+                  AppImages.loader,
+                  width: 38,
+                  height: 25,
+                ),
+                const Spacing.empty(),
+              ],
+            ),
+          ),
+        );
       },
     );
   }
