2353a3c79ca8ceb9f252f747eb2336e473eb88e6
Tobiloba <developertobi@gmail.com>
2022-07-08 21:06:16 +0100

    Fixed product list blank issue

--- lib/features/core/views/product_list_view.dart
+++ lib/features/core/views/product_list_view.dart
@@ -48,14 +48,18 @@ class _WishlistState extends ConsumerState<ProductListView>
   @override
   void initState() {
     // TODO: implement initState
-    _tabController = TabController(
-        length: ref.read(categoryNotifierProvider).storeCategories.length,
-        vsync: this);
-    _tabController.addListener(_handleTabChange);
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      ref
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await ref
+          .read(categoryNotifierProvider)
+          .fetchStoreCategories(storeId: widget.store.id.toString());
+      await ref
           .watch(productListNotifierProvider)
           .fetchStoreProducts(storeId: widget.store.id, category: 'all');
+      _tabController = TabController(
+          length: ref.read(categoryNotifierProvider).storeCategories.length,
+          vsync: this);
+      _tabController.addListener(_handleTabChange);
+      print('Left initttttttttttttttttttttttttt');
     });
     super.initState();
   }
@@ -107,151 +111,160 @@ class _WishlistState extends ConsumerState<ProductListView>
         centerTitle: true,
       ),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // const Spacing.mediumHeight(),
-            DecoratedBox(
-                decoration: BoxDecoration(
-                  //This is for background color
-                  color: Colors.white.withOpacity(0.0),
+        child: productListNotifier.state.isLoading ||
+                categoryNotifier.storeCategoriesLoading
+            ? const CircularProgress()
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // const Spacing.mediumHeight(),
+                  DecoratedBox(
+                      decoration: BoxDecoration(
+                        //This is for background color
+                        color: Colors.white.withOpacity(0.0),
 
-                  //This is for bottom border that is needed
-                  border: Border(
-                      bottom: BorderSide(color: AppColors.grey8, width: 2)),
-                ),
-                child: TabBar(
-                    indicatorColor: AppColors.primaryColor,
-                    labelColor: AppColors.shade5,
-                    unselectedLabelColor: AppColors.grey5,
-                    // padding: const EdgeInsets.only(bottom: 0),
-                    controller: _tabController,
-                    isScrollable: true,
-                    onTap: (index) {
-                      print('index $index');
-                      productListNotifier.fetchStoreProducts(
-                          storeId: widget.store.id,
-                          category: categoryNotifier.storeCategories[index]);
-                    },
-                    tabs: categoryNotifier.storeCategories
-                        .map((category) => Tab(text: category))
-                        .toList())),
-            Expanded(
-              child: TabBarView(
-                controller: _tabController,
-                children: categoryNotifier.storeCategories.map((category) {
-                  return productListNotifier.state.isLoading
-                      ? const CircularProgress()
-                      : productListNotifier.products.isEmpty
-                          ? AppEmptyStates(
-                              onButtonPressed: () {
-                                ref
-                                    .read(navigationServiceProvider)
-                                    .navigateToNamed(
-                                      Routes.addProduct,
-                                      arguments: widget.store,
-                                    );
-                              },
-                              imageString: AppImages.emptyProduct,
-                              message1String: 'No Product Added Yet',
-                              message2String:
-                                  'Tap the button below to create your first store',
-                              buttonString: '+ Add your first Product',
-                              hasButton: true,
-                              hasIcon: false,
-                              // onButtonPressed: () => homeNotifier.fetchProducts(category: 'all'),
-                            )
-                          : Padding(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 10,
-                                horizontal: 24,
-                              ),
-                              child: MasonryGridView.count(
-                                itemCount: productListNotifier.products.length,
-                                crossAxisCount: 2,
-                                mainAxisSpacing: 0,
-                                crossAxisSpacing: 16,
-                                itemBuilder: (context, index) {
-                                  return ProductContainerPList(
-                                    url: productListNotifier
-                                        .products[index].image[0],
-                                    storeName: productListNotifier
-                                        .products[index].store.name,
-                                    productName: productListNotifier
-                                        .products[index].name,
-                                    productPrice: productListNotifier
-                                        .products[index].price,
-                                    distance:
-                                        ref.read(locationService).getDistance(
-                                              endLat: productListNotifier
-                                                  .products[index].lat,
-                                              endLon: productListNotifier
-                                                  .products[index].lon,
-                                            ),
-                                    isFavorite: productListNotifier
-                                        .products[index].isFav!,
-                                    onProductTapped: () {
-                                      ref
-                                          .read(navigationServiceProvider)
-                                          .navigateToNamed(
-                                            Routes.productDetails2,
-                                            arguments: productListNotifier
-                                                .products[index],
-                                            // arguments: EditProductArgModel(store: productListNotifier.products[index].store, product: productListNotifier.productEditModel),
-                                          );
-                                      // ref
-                                      //     .read(navigationServiceProvider)
-                                      //     .navigateToNamed(
-                                      //       Routes.editProduct,
-                                      //       // arguments: productListNotifier.products[index],
-                                      //       // arguments: EditProductArgModel(store: productListNotifier.products[index].store, product: productListNotifier.productEditModel),
-                                      //     );
-                                    },
-                                    onDistanceTapped: () {},
-                                    onFlipTapped: () async {
-                                      await ref
-                                          .read(flipNotifierProvider)
-                                          .addItemToCompare(
-                                              productId: productListNotifier
-                                                  .products[index].id);
-                                      if (ref
-                                          .read(flipNotifierProvider)
-                                          .successfullyAdded) {
+                        //This is for bottom border that is needed
+                        border: const Border(
+                          bottom: BorderSide(
+                            color: AppColors.grey8,
+                            width: 2,
+                          ),
+                        ),
+                      ),
+                      child: TabBar(
+                          indicatorColor: AppColors.primaryColor,
+                          labelColor: AppColors.shade5,
+                          unselectedLabelColor: AppColors.grey5,
+                          // padding: const EdgeInsets.only(bottom: 0),
+                          controller: _tabController,
+                          isScrollable: true,
+                          onTap: (index) {
+                            print('index $index');
+                            productListNotifier.fetchStoreProducts(
+                                storeId: widget.store.id,
+                                category:
+                                    categoryNotifier.storeCategories[index]);
+                          },
+                          tabs: categoryNotifier.storeCategories
+                              .map((category) => Tab(text: category))
+                              .toList())),
+                  Expanded(
+                    child: TabBarView(
+                      controller: _tabController,
+                      children:
+                          categoryNotifier.storeCategories.map((category) {
+                        return productListNotifier.products.isEmpty
+                            ? AppEmptyStates(
+                                onButtonPressed: () {
+                                  ref
+                                      .read(navigationServiceProvider)
+                                      .navigateToNamed(
+                                        Routes.addProduct,
+                                        arguments: widget.store,
+                                      );
+                                },
+                                imageString: AppImages.emptyProduct,
+                                message1String: 'No Product Added Yet',
+                                message2String:
+                                    'Tap the button below to create your first store',
+                                buttonString: '+ Add your first Product',
+                                hasButton: true,
+                                hasIcon: false,
+                                // onButtonPressed: () => homeNotifier.fetchProducts(category: 'all'),
+                              )
+                            : Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 10,
+                                  horizontal: 24,
+                                ),
+                                child: MasonryGridView.count(
+                                  itemCount:
+                                      productListNotifier.products.length,
+                                  crossAxisCount: 2,
+                                  mainAxisSpacing: 0,
+                                  crossAxisSpacing: 16,
+                                  itemBuilder: (context, index) {
+                                    return ProductContainerPList(
+                                      url: productListNotifier
+                                          .products[index].image[0],
+                                      storeName: productListNotifier
+                                          .products[index].store.name,
+                                      productName: productListNotifier
+                                          .products[index].name,
+                                      productPrice: productListNotifier
+                                          .products[index].price,
+                                      distance:
+                                          ref.read(locationService).getDistance(
+                                                endLat: productListNotifier
+                                                    .products[index].lat,
+                                                endLon: productListNotifier
+                                                    .products[index].lon,
+                                              ),
+                                      isFavorite: productListNotifier
+                                          .products[index].isFav!,
+                                      onProductTapped: () {
                                         ref
                                             .read(navigationServiceProvider)
-                                            .navigateToNamed(Routes.compare);
-                                      }
-                                    },
-                                    onFavoriteTapped: () async {
-                                      productListNotifier.products[index].isFav!
-                                          ? await ref
-                                              .read(wishlistNotifierProvider)
-                                              .removeFromWishlist(
-                                                productId: ref
-                                                    .read(
-                                                        wishlistNotifierProvider)
-                                                    .products[index]
-                                                    .id,
-                                              )
-                                          : await ref
-                                              .read(wishlistNotifierProvider)
-                                              .addToWishlist(
-                                                productId: ref
-                                                    .read(
-                                                        wishlistNotifierProvider)
-                                                    .products[index]
-                                                    .id,
-                                              );
-                                      ref.refresh(wishlistNotifierProvider);
-                                    },
-                                  );
-                                },
-                              ));
-                }).toList(),
+                                            .navigateToNamed(
+                                              Routes.productDetails2,
+                                              arguments: productListNotifier
+                                                  .products[index],
+                                              // arguments: EditProductArgModel(store: productListNotifier.products[index].store, product: productListNotifier.productEditModel),
+                                            );
+                                        // ref
+                                        //     .read(navigationServiceProvider)
+                                        //     .navigateToNamed(
+                                        //       Routes.editProduct,
+                                        //       // arguments: productListNotifier.products[index],
+                                        //       // arguments: EditProductArgModel(store: productListNotifier.products[index].store, product: productListNotifier.productEditModel),
+                                        //     );
+                                      },
+                                      onDistanceTapped: () {},
+                                      onFlipTapped: () async {
+                                        await ref
+                                            .read(flipNotifierProvider)
+                                            .addItemToCompare(
+                                                productId: productListNotifier
+                                                    .products[index].id);
+                                        if (ref
+                                            .read(flipNotifierProvider)
+                                            .successfullyAdded) {
+                                          ref
+                                              .read(navigationServiceProvider)
+                                              .navigateToNamed(Routes.compare);
+                                        }
+                                      },
+                                      onFavoriteTapped: () async {
+                                        productListNotifier
+                                                .products[index].isFav!
+                                            ? await ref
+                                                .read(wishlistNotifierProvider)
+                                                .removeFromWishlist(
+                                                  productId: ref
+                                                      .read(
+                                                          wishlistNotifierProvider)
+                                                      .products[index]
+                                                      .id,
+                                                )
+                                            : await ref
+                                                .read(wishlistNotifierProvider)
+                                                .addToWishlist(
+                                                  productId: ref
+                                                      .read(
+                                                          wishlistNotifierProvider)
+                                                      .products[index]
+                                                      .id,
+                                                );
+                                        ref.refresh(wishlistNotifierProvider);
+                                      },
+                                    );
+                                  },
+                                ));
+                      }).toList(),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
       ),
     );
   }
